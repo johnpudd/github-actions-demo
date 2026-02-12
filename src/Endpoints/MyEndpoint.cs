542b664f8feccbc5affd2b37b316960b3e309cb9
John Puddifoot <john@pudd.co.uk>
2026-02-12 15:26:22 +0000

    Fix build

--- src/Endpoints/MyEndpoint.cs
+++ src/Endpoints/MyEndpoint.cs
@@ -14,7 +14,7 @@ public class MyEndpoint : Endpoint<MyRequest, MyResponse>
     {
         await Send.OkAsync(new()
         {
-            FullName = req.FirstName + (req.FirstName == "" ||  req.LastName == "") ? " " : "" + req.LastName,
+            FullName = req.FirstName + ((req.FirstName == "" ||  req.LastName == "") ? " " : "") + req.LastName,
             IsOver18 = req.Age >= 18
         });
     }

ab5018123ab336e06f2b95404f02d51c74dd411a
John Puddifoot <john@pudd.co.uk>
2026-02-12 13:45:37 +0000

    Simple c# app

---
+++ src/Endpoints/MyEndpoint.cs
@@ -0,0 +1,21 @@
+using FastEndpoints;
+
+public class MyEndpoint : Endpoint<MyRequest, MyResponse>
+{
+    public override void Configure()
+    {
+        var path = "/api/user/create";
+        Get(path);
+        Post(path);
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(MyRequest req, CancellationToken ct)
+    {
+        await Send.OkAsync(new()
+        {
+            FullName = req.FirstName + " " + req.LastName,
+            IsOver18 = req.Age > 18
+        });
+    }
+}
